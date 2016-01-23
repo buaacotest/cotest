9ab1a7c729dce181f026f84d80137f4219decb92
sparkingarthur <12061025@buaa.edu.cn>
2016-01-23 22:19:49 +0800

    LSJ：调整后端代码结构 init.php和details.php

--- includes/init.php
+++ includes/init.php
@@ -6,26 +6,14 @@
  * Time: 14:42
  */
 /*初始化数据库类*/
-require( '../sql/mysql_cls.php');
+require( './sql/mysql_cls.php');
 $db=new mysql_cls();
 $db->connect();
 $serverAddress=$serverUsername=$serverPassword=$selectDBname=NULL;
 
-/*test for LSJ's functions*/
-$dbnumber=$db->getDBnumber();
-echo $dbnumber;
-$dbnames=$db->getAllDBnames();
-print_r($dbnames);
-$db->changeDB("microwave");
-$dbsename=$db->getNowDB();
-echo $dbsename;
-
-require('../includes/lib_common.php');
+require('./includes/lib_common.php');
 /*载入Common语言文件*/
 $_CFG=load_config();
-require('../lang/'.$_CFG['lang'].'/common.php');
+require('./lang/'.$_CFG['lang'].'/common.php');
 
-/*test some lib_function by lsj*/
-require ('../includes/lib_products.php');
-$evaluations=getEvalutionsByOneID(1074);
-print_r($evaluations);
+
