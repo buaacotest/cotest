a344c7ff981644a11a224c11015b7e01ca6f303a
sparkingarthur <12061025@buaa.edu.cn>
2016-03-30 10:33:29 +0800

    1.修改取消selected的js 传递PHP的BUG,修改PHP提示

--- translation/saveitem.php
+++ translation/saveitem.php
@@ -13,40 +13,54 @@ $projectname=$_GET['projname'];////所翻译的数据库
 $chn=$_GET['CHN'];///翻译
 $flag=$_GET['flag'];////标示是否选择
 $type=$_GET['type'];/////翻译的是evaluation还是property
-if($chn=="")
-    echo "翻译内容为空!";
-else{
-    //echo $type;
-    $db=new mysql_cls();
-    $db->connect();
-    $serverAddress=$serverUsername=$serverPassword=$selectDBname=NULL;
-    $db->changeDB($projectname);
-    $transarr=array('CHN'=>$chn,'Eng'=>"null",'De'=>"null");///暂时De都为空
-    require('lib.php');
-    $result1=0;
-    $result2=0;
-    $outstr1="";
-    $outstr2="";
-    if($type=="property")
-      $dicflag=0;
-    if($type=="evaluation")
-      $dicflag=1;
-    if($flag==1){////选择的同时进行保存
+//echo $type;
+$db=new mysql_cls();
+$db->connect();
+$serverAddress=$serverUsername=$serverPassword=$selectDBname=NULL;
+$db->changeDB($projectname);
+$transarr=array('CHN'=>$chn,'Eng'=>"null",'De'=>"null");///暂时De都为空
+require('lib.php');
+$result1=0;
+$result2=0;
+$outstr1="";
+$outstr2="";
+
+if($type=="property")
+    $dicflag=0;
+if($type=="evaluation")
+    $dicflag=1;
+
+if($flag==1){/////选择保存
+    if($chn=="")//////TODO：取消选择的时候是否需要判断翻译为空不为空？
+        echo "翻译为空!";
+    else{
         $result1=SaveTranslationToAdminDic($oriword,$transarr);///一旦修改了input，说明是新增的意思
         $result2=SaveTranslationToSelfDic($oriword,$transarr,$id,$dicflag);
-
         if($result1)
-            $outstr1="saved in admindic!";
+            $outstr1="存入了总词典!";
         if($result2)
-            $outstr2="saved in selfdic!";
-        echo "保存成功!";
+            $outstr2="存入了项目词典!";
+        echo "成功保存!".$outstr1.$outstr2;
+        //////无论是选择保存还是取消都要对selected进行操作
+        if($dicflag==0)
+            $result3=SetPropertySelected($id,$flag);
+        else if($dicflag==1)
+            $result3=SetEvaluationSelected($id,$flag);
+        if($result3)
+            echo "标注selected成功!";
     }
+}
+else{
+    //////无论是选择保存还是取消都要对selected进行操作
     if($dicflag==0)
-       $result3=SetPropertySelected($id,$flag);
+        $result3=SetPropertySelected($id,$flag);
     else if($dicflag==1)
         $result3=SetEvaluationSelected($id,$flag);
     if($result3)
-        echo "";
-
-    $db->close();
+        echo "取消 selected成功!";
 }
+
+
+
+
+$db->close();
