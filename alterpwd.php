b1c4a91f703eacd9ce0f3f4b9b2273b911355c55
Runingtime <958919277@qq.com>
2016-04-17 20:31:34 +0800

    添加修改密码功能

---
+++ alterpwd.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: master
+ * Date: 2016/4/17
+ * Time: 20:08
+ */
+require('includes/init.php');
+include_once 'includes/config.php';
+require('lang/'.$_SESSION['lang'].'/user.php');
+$smarty->assign('lang',$_LANG);
+$smarty->display('alterpwd.tpl');
