fa9def2e96c355527b6e6efaf2446d7544e849e1
Runingtime <958919277@qq.com>
2016-06-25 10:33:24 +0800

    compare&footer_m

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
  * Date: 2016/4/17
  * Time: 15:22
  */
+require('includes/init.php');
 require('./lang/'.$_SESSION['lang'].'/common.php');
 $s = new Smarty;
 /*  定义各个目录的路径    */
@@ -17,4 +18,7 @@ $s->cache_dir = BASE_PATH.SMARTY_PATH.'cache/';
 $s->left_delimiter = '<{';
 $s->right_delimiter = '}>';
 $s->assign('lang',$_LANG);
-$s->display('footer.tpl');
+if(isMobile())
+    $s->display('footer_m.tpl');
+else
+    $s->display('footer.tpl');
