c341efc36ba777e886cee3e832e74ad2e4c7b1d8
sparkingarthur <sparkingarthur@163.com>
2017-02-03 17:00:40 +0800

    1.完善

--- cms/newssave.php
+++ cms/newssave.php
@@ -18,29 +18,41 @@ $title=trim($_POST['txtTitle']);
 $content=trim($_POST['content1']);
 
 $date='2017-01-25';
-print_r($txtid);
+print_r('txtid:'.$txtid);
 echo "</br>";
 
-print_r($category);
+print_r('category:'.$category);
 echo "</br>";
-print_r($product);
+print_r('product:'.$product);
 echo "</br>";
 
-print_r($title);
+print_r('title:'.$title);
 echo "</br>";
-print_r($content);
+print_r('content:'.$content);
+$result=0;
 if($category=='testprogramme'){
     if(!empty($txtid)){////如果 有id，则是更新
         $result=updateTestprogramme($txtid,$title,$content,$date,$product);
     }else{////否则是新建
-        
+        $result=addTestprogramme($title,$content,$date,$product);
     }
 
 }elseif($category=='testreport'){
+    if(!empty($txtid)){////如果 有id，则是更新
+        $result=updateTestreports($txtid,$title,$content,$date,$product);
+    }else{////否则是新建
+        $result=addTestreport($title,$content,$date,$product);
+    }
 
 }elseif($category=='cotestreport'){
+    if(!empty($txtid)){////如果 有id，则是更新
+        $result=updateCotestreports($txtid,$title,$content,$date);
+    }else{////否则是新建
+        $result=addCotestreport($title,$content,$date);
+    }
 
 }
-
-
-
+echo '</br>';
+if($result){
+    echo "save success!";
+}
