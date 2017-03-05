1f9fac9c2d33ae3f9ef888b6290d8e5f7a559ed4
sparkingarthur <sparkingarthur@163.com>
2017-03-05 15:29:01 +0800

    1.展示一篇新闻

---
+++ shownews.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arthur
+ * Date: 2017/3/5
+ * Time: 15:20
+ */
+
+require('includes/init.php');
+include_once 'includes/config.php';
+require('cms/libnewseditor.php');
+
+$newsid=trim($_GET['newsid']);
+$category=trim($_GET['category']);
+
+$result=[];
+
+if($category=='testprogramme'){
+        $result=getOneTestprogrammeByID($newsid);
+}else if($category=='testreport'){
+        $result=getOneTestreportByID($newsid);
+}else if($category=='cotestreport'){
+        $result=getOneCotestreportByID($newsid);
+}
+
+$title= $result['title'];
+$content = $result['content'];
+$date=$result['date'];
+
+//print_r($result);
+$smarty->assign('title',$title);
+$smarty->assign('date',$date);
+$smarty->assign('content',$content);
+
